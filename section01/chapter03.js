edc0bfdc8a19900783c82ce1b42890a24c6407cd
Yang Dayeon <ydy229@naver.com>
2025-04-23 23:45:32 +0900

    [JS] Ch03,04,05 추가 및 index 파일 생성
    1.1-1.6

---
+++ section01/chapter03.js
@@ -0,0 +1,24 @@
+console.log("hi");
+
+
+//1. 변수
+let age;
+age =30;
+
+//2. 상수
+const birth = "1992.02.23";
+
+//3. 변수 명명 규칙(네이밍 규칙)
+
+//3-1. $, _제외한 기호는 사용할 수 없다.
+let $_name;
+
+//3-2. 숫자로 시작할 수 없다.
+let name1;
+let name2;
+
+
+//4. 변수 명명 가이드
+let salesCount =1;
+let refundCount =1;
+let totalSalesCount = salesCount -refundCount;
---
+++ section01/chapter05.js
@@ -0,0 +1,8 @@
+//1. 묵시적 형 변환
+//-> 자바스크립트 엔진이 알아서 형 변환 하는 것
+
+let num = 10;
+let str = "20";
+
+const result = num +str;
+console.log(result); //1020
