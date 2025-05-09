6683d3608048106a74057dc73782be6afab11a77
Yang Dayeon <ydy229@naver.com>
2025-05-09 17:37:40 +0900

    [Section 05] Button.jsx 이벤트 처리

--- section05/src/App.jsx
+++ section05/src/App.jsx
@@ -1,29 +1,26 @@
-import './App.css'
-import Header from "./components/Header.jsx"; // 파일 확장자 .jsx 생략 가능
-import Footer from "./components/Footer.jsx";
-import Main from "./components/Main.jsx";
-import Button from"./components/Button.jsx";
+import "./App.css";
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Footer from "./components/Footer";
+import Button from "./components/Button";
 
-function App() { // 부모 컨포넌트
-
-  const buttonProps={
+function App() {
+  const buttonProps = {
     text: "메일",
     color: "red",
-    a:1,
-    b:2,
-    c:3,
+    a: 1,
+    b: 2,
+    c: 3,
   };
-
   return (
     <>
-      <Button {...buttonProps}/>
-      <Button text={"카페"}/>
+      <Button {...buttonProps} />
+      <Button text={"카페"} />
       <Button text={"블로그"}>
-        <div>자식 요소</div>
+        <Header />
       </Button>
-
     </>
-  )
+  );
 }
 
 export default App;
--- section05/src/components/Button.jsx
+++ section05/src/components/Button.jsx
@@ -1,10 +1,26 @@
-// '구조 분해 할당'으로 가져옴. 기본값="black"
-const Button =({children, text, color ="black"})=>{
-
-return <button style={{color: color}}>
-    {text} - {color.toUpperCase()}
-    {children}
-    </button>;
-};
-
-export default Button;
+const Button = ({ text, color="black", children }) => {
+    // 이벤트 객체 (SyntheticBaseEvent- 콘솔창)
+    const onClickButton = (e) => {
+      console.log(e);
+      console.log(text);
+    };
+  
+    return (
+      <button
+        onClick={onClickButton}
+        //onMouseEnter={onClickButton}
+        style={{ color: color }}
+      >
+        {text} - {color.toUpperCase()}
+        {children}
+      </button>
+    );
+  };
+  
+  /* React 19버전 이후부터는 무시되는 경향ㅇ
+  Button.defaultProps = {
+    color: "black",
+  };
+  */
+  
+  export default Button;
