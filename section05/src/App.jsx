0a0e7fe5b09da66bdb23afcd862c28d91b9136d8
Yang Dayeon <ydy229@naver.com>
2025-05-11 16:04:25 +0900

    [Section 05] Hook과 CustomHook, useRef, useState 추가
    Custom hook 인 useInput.jsx 분리및 추가함.

--- section05/src/App.jsx
+++ section05/src/App.jsx
@@ -8,7 +8,6 @@ function App() {
   return(
     <>
       <HookExam/>
-      <Register/>
     </>
   );
 }
--- section05/src/components/HookExam.jsx
+++ section05/src/components/HookExam.jsx
@@ -1,14 +1,21 @@
-import { useState } from "react";
-
+import useInput from "./../hooks/useInput";
 // 3가지 hook 관련된 팁
 // 1. 함수 텀포넌트, 커스텀 훅 내부에서만 호출 가능
 // 2. 조건부로 호출될 수는 없다
-    
-const HookExam =() =>{
-    
-    const state = useState(); // 내부에서 호출 ㅇ
+// 3. 나만의 훅(Cutsom Hook)을 직접 만들 수 있다.
+// 커스텀 훅 만드는법: 함수 앞에 use를 붙인다. ex) useInput()
+// 커스텀 훅: 보통 components가 아닌, 'hooks'폴더 생성 후 함수 이름으로 보관함.
 
-    return <div>hookexam</div>
+
+
+const HookExam =() =>{
+    const [input, onChange] = useInput();
+
+    return (
+        <div>
+            <input value ={input} onChange={onChange}/>
+        </div>
+    )
 };
 
-export default Hookexam;
+export default HookExam;
