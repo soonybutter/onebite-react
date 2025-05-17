1a0591fb2eea17f1dcc50d1d2f06b2fdac118ea9
Yang Dayeon <ydy229@naver.com>
2025-05-17 16:15:56 +0900

    [Section 08] todoList 수정, 삭제 기능 추가

--- section08/src/App.jsx
+++ section08/src/App.jsx
@@ -42,14 +42,36 @@ function App() {
 
     // setTodos 호출하여 인수로 새로운 값을 newTodo에 넣음.
     setTodos([newTodo, ...todos]);
+  };
+
+  const onUpdate = (targetId)=>{
+    // todos State의 값들 중에
+    // targetId와 일치하는 id를 갖는 투두 아이템의 isDone 변경
+
+    // 인수: todos 배열에서 targetID와 일치하는 id를 갖는 요소의 데이터만 딱 바꾼 새로운 배열 전달
+    setTodos(todos.map((todo)=>{
+      if(todo.id=== targetId){
+        return{
+          ...todo,
+          isDone: !todo.isDone
+        }
+      }
+      return todo
+    }))
   }
+
+  // 삭제 함수
+  const onDelete =(targetId)=>{
+    // 인수: todos 배열에서 targetId와 일치하는 id를 갖는 요소만 삭제한 새로운 배열
+    setTodos(todos.filter((todo)=>todo.id !== targetId));
+  };
 
   // Editor 컴포넌트에 onCreate 함수 전달
   return (
     <div className="App">
       <Header/>
       <Editor onCreate={onCreate}/>
-      <List todos ={todos}/>
+      <List todos ={todos} onUpdate={onUpdate} onDelete={onDelete}/>
     </div>
   )
    
