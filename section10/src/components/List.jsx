c8c26a9dfddd9f53a6f9b7148730e0887aec21b3
Yang Dayeon <ydy229@naver.com>
2025-05-21 15:54:11 +0900

    [Section 10] useMemo 사용, GetAnalyzedData 기능 추가
    - useMemo hook
    - total, done, notDone 으로 분류된 count 횟수 기능 추가함.

---
+++ section10/src/components/List.jsx
@@ -0,0 +1,73 @@
+import "./List.css";
+import TodoItem from "./TodoItem";
+import { useState, useMemo } from "react";
+
+const List =({ todos, onUpdate, onDelete })=>{
+
+    const [search, setSearch] = useState("");
+
+    const onChangeSearch = (e) =>{
+        setSearch (e.target.value);
+    };
+
+    const getFilteredData =() =>{
+        if(search===""){
+            return todos;
+        }
+        return todos.filter((todo)=>
+            todo.content.toLowerCase().includes(search.toLowerCase()));
+    };
+
+    const FilterdTodos = getFilteredData();
+
+
+    // 현재 todoList 분석하는 함수 - useMemo 사용
+    // 딱 한번만 수행되도록!
+    const {totalCount, doneCount, notDoneCount}= useMemo(()=>{
+        console.log("getAnalyzedData 호출!");
+        const totalCount = todos.length;
+        const doneCount = todos.filter(
+            (todo) => todo.isDone
+        ). length;
+
+        const notDoneCount = totalCount - doneCount;
+
+        return {
+            totalCount,
+            doneCount,
+            notDoneCount
+        };
+    }, [todos])
+    // [] : 의존성배열 deps
+    // []안에 아무것도 없으면, 콜백함수의 연산수행 및 반환은 최초 실행 시 딱 한번 반환됨.
+
+    return(
+        <div className="List">
+            <h4>Todo List🌱</h4>
+            <div>
+                <div>total: {totalCount}</div>
+                <div>done: {doneCount}</div>
+                <div>notDone: {notDoneCount}</div>
+            </div>
+            <input 
+            value={search}
+            onChange={onChangeSearch}
+            placeholder="검색어를 입력하세요"/>
+
+            <div className="todos_wrapper">
+               {FilterdTodos.map((todo)=>{
+                    return (
+                    <TodoItem
+                     key={todo.id}
+                     {...todo}
+                     onUpdate={onUpdate}
+                     onDelete={onDelete}
+                    />
+                    )
+               })}
+            </div>
+        </div>
+    );
+};
+
+export default List;
