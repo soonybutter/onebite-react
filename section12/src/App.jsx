3833b94d55ebea694d6b6dfc4198838126e0c122
Yang Dayeon <ydy229@naver.com>
2025-05-27 12:37:29 +0900

    [Section 12] {감정일기장 프로젝트} 이미지파일 추가, css 수정
    - 프론트 작업 진행
    - 이미지 import  위한 util 폴더 생성

--- section12/src/App.jsx
+++ section12/src/App.jsx
@@ -5,6 +5,9 @@ import Diary from "./pages/Diary";
 import New from "./pages/New";
 import NotFound from "./pages/NotFound";
 
+import { getEmotionImage } from "./util/get-emotion-image.js";
+
+
 // 1. "/" : 모든 일기를 조회하는 Home 페이지
 // 2. "/new" : 새로운 일기를 작성하는 New 페이지
 // 3. "/diary" : 일기를 상세히 조회하는 Diary 페이지 
@@ -18,6 +21,14 @@ function App() {
 
   return(
     <>
+
+      <div>
+        <img src={getEmotionImage(1)}/>
+        <img src={getEmotionImage(2)}/>
+        <img src={getEmotionImage(3)}/>
+        <img src={getEmotionImage(4)}/>
+        <img src={getEmotionImage(5)}/>
+      </div>
       <div>
         <Link to={"/"}>Home</Link>
         <Link to={"/new"}>New</Link>
