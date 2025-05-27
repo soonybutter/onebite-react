28ee313ededb9216e089ca0baeddc39ce8b98602
Yang Dayeon <ydy229@naver.com>
2025-05-27 13:17:20 +0900

    [Section 12] {감정일기장 프로젝트} Header, 상단 버튼 추가
    -상단 button 추가

--- section12/src/App.jsx
+++ section12/src/App.jsx
@@ -4,6 +4,8 @@ import Home from "./pages/Home";
 import Diary from "./pages/Diary";
 import New from "./pages/New";
 import NotFound from "./pages/NotFound";
+import Button from "./components/Button.jsx";
+import Header from "./components/Header.jsx";
 
 import { getEmotionImage } from "./util/get-emotion-image.js";
 
@@ -21,21 +23,27 @@ function App() {
 
   return(
     <>
+      <Header 
+        title={"Header"} 
+        leftChild={<Button text={"Left"}/>} 
+        rightChild={<Button text={"Right"}/>} 
+      />
+      <Button 
+        text={"123"} 
+        type={"POSITIVE"}
+        onClick={()=>{
+          console.log("123 버튼 클릭! ");
+        }}
+      />
 
-      <div>
-        <img src={getEmotionImage(1)}/>
-        <img src={getEmotionImage(2)}/>
-        <img src={getEmotionImage(3)}/>
-        <img src={getEmotionImage(4)}/>
-        <img src={getEmotionImage(5)}/>
-      </div>
-      <div>
-        <Link to={"/"}>Home</Link>
-        <Link to={"/new"}>New</Link>
-        <Link to={"/diary"}>Diary</Link>
-        
-      </div>
-      <button onClick={onClickButton}>New 페이지로 이동</button>
+      <Button 
+        text={"123"} 
+        type={"NEGATIVE"}
+        onClick={()=>{
+          console.log("123 버튼 클릭! ");
+        }}
+      />
+
       <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path="/new" element={<New/>}/>
---
+++ section12/src/components/Button.jsx
@@ -0,0 +1,13 @@
+import "./Button.css";
+
+const Button=({ text, type, onClick })=>{
+    return  (
+            <button onClick={onClick} 
+             className={`Button Button_${type}`}
+            >
+             {text}
+            </button>
+    );
+};
+
+export default Button;
