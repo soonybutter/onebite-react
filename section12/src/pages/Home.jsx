c801f55188785345ceaed2692bb950de218d860c
Yang Dayeon <ydy229@naver.com>
2025-05-28 13:47:45 +0900

    [Section 12] {감정일기장 프로젝트} DiaryList 구현
    -DiaryList.jsx, css 추가
    -DiaryItem.jsx, css 추가

--- section12/src/pages/Home.jsx
+++ section12/src/pages/Home.jsx
@@ -1,10 +1,18 @@
-import { useSearchParams } from "react-router-dom";
+import Header from "../components/Header";
+import Button from "../components/Button";
+import DiaryList from "../components/DiaryList";
 
 const Home =()=>{
-    const [params, setParams] = useSearchParams();
-    console.log(params.get("value"));
 
-    return<div>Home</div>;
+    return(
+    <div>
+        <Header title={"2025년 5월"}
+            leftChild={<Button text={"<"}/>}
+            rightChild={<Button text={">"}/>}
+        />
+        <DiaryList/>
+    </div>
+    );
 };
 
 export default Home;
--- section12/src/pages/NotFound.jsx
+++ section12/src/pages/NotFound.jsx
@@ -1,5 +1,5 @@
 const NotFound =()=>{
-    return <div>잘못된 페이지입니다.</div>
+    return <div>⚠️ 잘못된 페이지입니다. </div>
 }
 
 export default NotFound;
