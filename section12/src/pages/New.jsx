b9d29dcff4e14834ff3a56b4b1393731aafbd060
Yang Dayeon <ydy229@naver.com>
2025-05-30 13:17:31 +0900

    Revert "[Section 12] {감정일기장 프로젝트} 일기쓰기 페이지 추가"
    This reverts commit 938caa176adb91fcf30a1b0782e732327a9318b8.

--- section12/src/pages/New.jsx
+++ section12/src/pages/New.jsx
@@ -1,14 +1,5 @@
-import Header from "../components/Header";
-import Button from "../components/Button";
-import Editor from "../components/Editor";
-
 const New =()=>{
-    return(
-        <div>
-            <Header title={"새 일기 쓰기"} leftChild={<Button text={"< 뒤로 가기"}/>}/>
-            <Editor />
-        </div>
-    );
+    return<div>New</div>;
 };
 
 export default New;
