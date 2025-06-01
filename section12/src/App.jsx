eaebb7b2571ce6c78f7ba68d04889c36c7e47d78
Yang Dayeon <ydy229@naver.com>
2025-06-01 16:53:02 +0900

    [Section 12] {감정일기장 프로젝트} 웹 스토리지 저장 기능 추가
    - 웹 스토리지에 데이터 저장되도록 구현
    - 더미데이터 삭제

--- section12/src/App.jsx
+++ section12/src/App.jsx
@@ -1,139 +1,145 @@
-import './App.css'
-import { Routes, Route, Link , useNavigate } from "react-router-dom"; 
-import { useReducer, useRef, createContext } from "react";
+import "./App.css";
+import {
+  useReducer,
+  useRef,
+  createContext,
+  useEffect,
+  useState,
+} from "react";
+import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Diary from "./pages/Diary";
 import New from "./pages/New";
-import NotFound from "./pages/NotFound";
-import Edit from "./pages/Edit.jsx"
-import Button from "./components/Button.jsx";
-import Header from "./components/Header.jsx";
+import Edit from "./pages/Edit";
+import Notfound from "./pages/Notfound";
 
-import { getEmotionImage } from "./util/get-emotion-image.js";
+function reducer(state, action) {
+  let nextState;
 
-
-// 1. "/" : 모든 일기를 조회하는 Home 페이지
-// 2. "/new" : 새로운 일기를 작성하는 New 페이지
-// 3. "/diary" : 일기를 상세히 조회하는 Diary 페이지 
-
-const mockData =[
-  {
-    id: 1,
-    createdDate: new Date("2025-05-29").getTime(),
-    emotionId:1,
-    content:"1번 일기 내용",
-  },
-  {
-    id: 2,
-    createdDate: new Date("2025-05-28").getTime(),
-    emotionId: 2,
-    content: "2번 일기 내용",
-  },
-  {
-    id: 3,
-    createdDate: new Date("2025-04-12").getTime(),
-    emotionId: 3,
-    content: "3번 일기 내용",
-  },
-  {
-    id: 4,
-    createdDate: new Date("2025-01-01").getTime(),
-    emotionId: 4,
-    content: "4번 일기 내용",
-  },
-  {
-    id: 5,
-    createdDate: new Date("2025-02-27").getTime(),
-    emotionId: 5,
-    content: "5번 일기 내용",
-  },
-
-];
-
-function reducer(state, action){
-  switch(action.type){
-    case 'CREATE': 
-      return[action.data,...state];
-    case 'UPDATE':
-      return state.map((item)=>
-        String(item.id) === String(action.data.id)? action.data : item
-      ); 
-    case 'DELETE':
-      return state.filter((item)=> String(item.id) !== String(action.id));  
+  switch (action.type) {
+    case "INIT":
+      return action.data;
+    case "CREATE": {
+      nextState = [action.data, ...state];
+      break;
+    }
+    case "UPDATE": {
+      nextState = state.map((item) =>
+        String(item.id) === String(action.data.id)
+          ? action.data
+          : item
+      );
+      break;
+    }
+    case "DELETE": {
+      nextState = state.filter(
+        (item) => String(item.id) !== String(action.id)
+      );
+      break;
+    }
     default:
       return state;
   }
+
+  localStorage.setItem("diary", JSON.stringify(nextState));
+  return nextState;
 }
 
 export const DiaryStateContext = createContext();
 export const DiaryDispatchContext = createContext();
 
-
-
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [data, dispatch] = useReducer(reducer, []);
+  const idRef = useRef(0);
 
-  const [data, dispatch] = useReducer(reducer, mockData);
-  const idRef = useRef(3);
+  useEffect(() => {
+    const storedData = localStorage.getItem("diary");
+    if (!storedData) {
+      setIsLoading(false);
+      return;
+    }
+
+    const parsedData = JSON.parse(storedData);
+    if (!Array.isArray(parsedData)) {
+      setIsLoading(false);
+      return;
+    }
+
+    let maxId = 0;
+    parsedData.forEach((item) => {
+      if (Number(item.id) > maxId) {
+        maxId = item.id;
+      }
+    });
+
+    idRef.current = maxId + 1;
+
+    dispatch({
+      type: "INIT",
+      data: parsedData,
+    });
+    setIsLoading(false);
+  }, []);
 
   // 새로운 일기 추가
-  const onCreate =(createdDate, emotionId, content) =>{
+  const onCreate = (createdDate, emotionId, content) => {
     dispatch({
-      type:"CREATE",
+      type: "CREATE",
       data: {
         id: idRef.current++,
         createdDate,
         emotionId,
         content,
       },
-    })
-  }
-  // 기존 일기 수정
-  const onUpdate =(id, createdDate, emotionId, content)=>{
-      dispatch(
-        {
-          type: "UPDATE",
-          data: {
-            id,
-            createdDate,
-            emotionId,
-            content,
-          },
-        });
+    });
   };
-  
+
+  // 기존 일기 수정
+  const onUpdate = (id, createdDate, emotionId, content) => {
+    dispatch({
+      type: "UPDATE",
+      data: {
+        id,
+        createdDate,
+        emotionId,
+        content,
+      },
+    });
+  };
+
   // 기존 일기 삭제
-  const onDelete=(id)=>{
+  const onDelete = (id) => {
     dispatch({
       type: "DELETE",
       id,
     });
   };
 
-  const nav = useNavigate();
+  if (isLoading) {
+    return <div>데이터 로딩중입니다 ...</div>;
+  }
 
-  const onClickButton =() =>{
-    nav("/new");
-  };
-
-  return(
+  return (
     <>
-     
       <DiaryStateContext.Provider value={data}>
-        <DiaryDispatchContext.Provider value={{
-          onCreate,onUpdate,onDelete,
-        }}>
+        <DiaryDispatchContext.Provider
+          value={{
+            onCreate,
+            onUpdate,
+            onDelete,
+          }}
+        >
           <Routes>
-            <Route path="/" element={<Home/>}/>
-            <Route path="/new" element={<New/>}/>
-            <Route path="/diary/:id" element={<Diary/>}/>
-            <Route path="/edit/:id" element={<Edit/>}/>
-            <Route path="*" element={<NotFound/>}/>
+            <Route path="/" element={<Home />} />
+            <Route path="/new" element={<New />} />
+            <Route path="/diary/:id" element={<Diary />} />
+            <Route path="/edit/:id" element={<Edit />} />
+            <Route path="*" element={<Notfound />} />
           </Routes>
         </DiaryDispatchContext.Provider>
       </DiaryStateContext.Provider>
-      
     </>
-  
   );
 }
 
